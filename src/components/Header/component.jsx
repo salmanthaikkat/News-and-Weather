b36df767d23449a6ad5686e05a2cce8198a814d9
Salman Thaikat <salmanthaikkat@gmail.com>
2020-08-19 19:57:09 +0530

    Initial commit

---
+++ src/components/Header/component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="app-header">
+      <div className="app-header__title">
+        News Corner
+      </div>
+      <div className="app-header__lang">
+        EN
+      </div>
+    </div>
+  )
+}
